905afea446d10d4d94b21caf7063a59fa7cd9ce8
D3v1s0m <d3v1s0m@gmail.com>
2025-07-28 18:19:36 +0530

    1.21.5 click and hover event fix

--- src/main/java/com/fexl/viewlock/mixin/PlayerTeleport.java
+++ src/main/java/com/fexl/viewlock/mixin/PlayerTeleport.java
@@ -62,20 +62,20 @@ public class PlayerTeleport {
 				.append(MutableComponent.create(new LiteralContents("View unlocked! Click "))
 						.withStyle(style -> style
 								.withColor(ChatFormatting.RED)
-								.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, MutableComponent.create(PlainTextContents.EMPTY).append(hoverText)))
+								.withHoverEvent(new HoverEvent.ShowText(MutableComponent.create(PlainTextContents.EMPTY).append(hoverText)))
 						)
 				)
 				.append(MutableComponent.create(new LiteralContents("here"))
 						.withStyle(style -> style
 								.withColor(ChatFormatting.BLUE)
-								.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/vl relock"))
-								.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, MutableComponent.create(PlainTextContents.EMPTY).append("Reset view")))
+								.withClickEvent(new ClickEvent.RunCommand("/vl relock"))
+								.withHoverEvent(new HoverEvent.ShowText(MutableComponent.create(PlainTextContents.EMPTY).append("Reset view")))
 						)
 				)
 				.append(MutableComponent.create(new LiteralContents(" to restore lock."))
 						.withStyle(style -> style
 								.withColor(ChatFormatting.RED)
-								.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, MutableComponent.create(PlainTextContents.EMPTY).append(hoverText)))
+								.withHoverEvent(new HoverEvent.ShowText(MutableComponent.create(PlainTextContents.EMPTY).append(hoverText)))
 						)
 				)
 		, false);
